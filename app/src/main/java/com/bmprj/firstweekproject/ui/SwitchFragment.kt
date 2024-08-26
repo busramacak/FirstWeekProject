e7b0eeaf84cbf7dc57da4a1cd0bbab03dc6ee1ee
Busra <busramacak@outlook.com>
2024-08-26 20:57:51 +0300

    Added Navigation library and configured main navigation in nav.xml. Created main_menu.xml for menu options. Connected to MainActivity. Updated EGO switch logic to control other switches' click events based on its state.

--- app/src/main/java/com/bmprj/firstweekproject/ui/SwitchFragment.kt
+++ app/src/main/java/com/bmprj/firstweekproject/ui/SwitchFragment.kt
@@ -1,17 +1,46 @@
 package com.bmprj.firstweekproject.ui
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.bmprj.firstweekproject.R
 import com.bmprj.firstweekproject.base.BaseFragment
 import com.bmprj.firstweekproject.databinding.FragmentSwitchBinding
 
 
 class SwitchFragment : BaseFragment<FragmentSwitchBinding>(FragmentSwitchBinding::inflate) {
     override fun setUpViews() {
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        with(binding){
+            skySwitch.isEnabled=false
+            moonSwitch.isEnabled=false
+            oceanSwitch.isEnabled=false
+            flameswitch.isEnabled=false
+            starSwitch.isEnabled=false
+
+            egoSwitch.setOnCheckedChangeListener { compoundButton, isChecked ->
+                if(isChecked){
+                    skySwitch.isEnabled=false
+                    moonSwitch.isEnabled=false
+                    oceanSwitch.isEnabled=false
+                    flameswitch.isEnabled=false
+                    starSwitch.isEnabled=false
+
+                    skySwitch.isChecked=false
+                    moonSwitch.isChecked=false
+                    oceanSwitch.isChecked=false
+                    flameswitch.isChecked=false
+                    starSwitch.isChecked=false
+                }else{
+
+
+                    skySwitch.isEnabled=true
+                    moonSwitch.isEnabled=true
+                    oceanSwitch.isEnabled=true
+                    flameswitch.isEnabled=true
+                    starSwitch.isEnabled=true
+                }
+            }
+        }
 
     }
 
