af8557d8ac67eaf0ddfe81ad3ebea492d79ba832
Busra <busramacak@outlook.com>
2024-08-26 16:50:45 +0300

    create SwitchFragment & setting xml, add string in strings.xml

---
+++ app/src/main/java/com/bmprj/firstweekproject/ui/SwitchFragment.kt
@@ -0,0 +1,18 @@
+package com.bmprj.firstweekproject.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bmprj.firstweekproject.R
+import com.bmprj.firstweekproject.base.BaseFragment
+import com.bmprj.firstweekproject.databinding.FragmentSwitchBinding
+
+
+class SwitchFragment : BaseFragment<FragmentSwitchBinding>(FragmentSwitchBinding::inflate) {
+    override fun setUpViews() {
+
+    }
+
+}
