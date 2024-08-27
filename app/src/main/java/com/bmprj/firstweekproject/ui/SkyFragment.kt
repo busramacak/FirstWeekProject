40926449fc0b9cc2e27618f986bdccbc37a39cac
Busra <busramacak@outlook.com>
2024-08-27 12:55:38 +0300

    create SkyFragment.kt & add navigation

---
+++ app/src/main/java/com/bmprj/firstweekproject/ui/SkyFragment.kt
@@ -0,0 +1,11 @@
+package com.bmprj.firstweekproject.ui
+
+import com.bmprj.firstweekproject.base.BaseFragment
+import com.bmprj.firstweekproject.databinding.FragmentSkyBinding
+
+class SkyFragment : BaseFragment<FragmentSkyBinding>(FragmentSkyBinding::inflate) {
+    override fun setUpViews() {
+
+    }
+
+}
